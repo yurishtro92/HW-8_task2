a401869a13a714159dc54c3c0c66b2c2b7133cd5
Yuri Shtro <yuri.schtro@gmail.com>
2021-07-10 13:50:44 +0300

    YaUploader created

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from pprint import pprint
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)}
+
+    def get_upload_link(self, file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        # pprint(response.json())
+        return response.json()
+
+    def upload(self, file_path, filename):
+        href = self.get_upload_link(file_path=file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code != 201:
+            print("Download error")
+        return print("Success")
+
+if __name__ == '__main__':
+    uploader = YaUploader('my_token_here')
+    result = uploader.upload(file_path = 'Новая папка/text.txt', filename = 'text.txt')
